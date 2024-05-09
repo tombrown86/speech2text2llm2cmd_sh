6a4ca07e5e536648c5aa40d3c7d8fa7ad0adf991
Tom Brown <tombrown86@gmail.com>
2024-05-09 17:24:05 +0100

    Use llama.cpp

--- s2cmd.sh
+++ s2cmd.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+GCLOUD_PROJ_NAME="pivotal-surfer-422716-p5"
+MODEL="../../AI/codellama-7b-instruct.Q8_0.gguf"
+BIN="/Users/tom/Code/AI/llama.cpp/main"
+
 echo "Recording audio... end with crl+c"
 
 sox -d recording.wav
-gcloud auth application-default login  # Authenticate with Google Cloud
-#gcloud auth application-default set-quota-project xxx
-gcloud config set project xxx
+gcloud config set project $GCLOUD_PROJ_NAME
 ffmpeg -i recording.wav recordingclean.wav
 
 llmprompt=`gcloud ml speech recognize recordingclean.wav --language-code=en-US --format=json | jq -r '.results[].alternatives[].transcript'`
@@ -14,24 +16,16 @@ rm -f recording.wav
 rm -f recordingclean.wav
 
 echo "Your LLM Prompt: $llmprompt"
-
-
-instruction="###Instruction:$llmprompt"
-
-model="" # Set the path to your model (gpt-2 or llama etc), like: model="/Volumes/ext1tb/Models/Other_Models/GPT-2/GPT-2_Large_774M/ggml-model-gpt-2-774M_q80.bin"
-
-infer="" # Set the path to your llama.cpp main or ggml gpt-2 etc, like: infer="./gpt-2"
-
-
-prompt="$prompt$user_input"
-
-
 echo "\n$llmprompt" >> text2llm2cmd.history
 
-tempfile=$(mktemp)
-instruction >> tempfile
 
-command="$($infer 2>/dev/null --top_p 0.3 --temp 0.5 -n 23 -b 16 -t 3 -m "$model" -f "$tempfile" | sed -n '71p')"
+instruction="###Instruction:Print me a linux command to: $llmprompt"
+tempfile=$(mktemp)
+echo "$instruction" > $tempfile
+
+
+# command using llama.cpp method
+command="$($BIN -m "$MODEL" -c 4096 --temp 0.5 --repeat_penalty 1.1 --file $tempfile)"
 
 echo "$command"
 read -p "Execute above command? (y/n) " -n 1 -r
