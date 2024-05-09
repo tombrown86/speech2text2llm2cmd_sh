4f0b21c284b843c1fb80d455f296267b077088ab
tombrown86 <tombrown86@gmail.com>
2024-05-09 10:23:25 +0100

    Update s2cmd.sh

--- s2cmd.sh
+++ s2cmd.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 echo "Recording audio... end with crl+c"
+
 sox -d recording.wav
 gcloud auth application-default login  # Authenticate with Google Cloud
 #gcloud auth application-default set-quota-project xxx
 gcloud config set project xxx
 ffmpeg -i recording.wav recordingclean.wav
+
 llmprompt=`gcloud ml speech recognize recordingclean.wav --language-code=en-US --format=json | jq -r '.results[].alternatives[].transcript'`
+
 rm -f recording.wav
 rm -f recordingclean.wav
 
@@ -30,11 +33,10 @@ instruction >> tempfile
 
 command="$($infer 2>/dev/null --top_p 0.3 --temp 0.5 -n 23 -b 16 -t 3 -m "$model" -f "$tempfile" | sed -n '71p')"
 
-
 echo "$command"
-
 read -p "Execute above command? (y/n) " -n 1 -r
-echo # change to a new line
+echo 
+
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     eval "$command"
